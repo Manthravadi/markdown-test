7907d39c0d4bdb46b3d2720d18ff3edb324b96e9
hsmanthr <harsha.manthravadi@hexagon.com>
2025-10-11 18:28:16 +0530

    feat: added new configs for the urls and titles of solutions card to make changes in one place and enable easier updates for future

---
+++ src/config/whySolutions.ts
@@ -0,0 +1,18 @@
+export default {
+  titleText: "Why?",
+  imgUrl: "/img/pages/common/chip.svg",
+  solutions: [
+    {
+      text: "Low Power",
+      url: "https://github.com/bnkcreativelab/sample-repository/blob/main/low-power.md",
+    },
+    {
+      text: "Security",
+      url: "https://github.com/bnkcreativelab/sample-repository/blob/main/security.md",
+    },
+    {
+      text: "Reliability",
+      url: "https://github.com/bnkcreativelab/sample-repository/blob/main/reliability.md",
+    },
+  ],
+};
